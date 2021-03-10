d09721edf0d08a5bf956fd6342b4c2a2b2a1c4f0
Takaharayuuki <yuuki.barber@gmail.com>
2021-03-10 16:15:23 +0900

    ベースのページ追加　完了

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer />
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  
+})
+</script>
+
+<style>
+
+</style>
