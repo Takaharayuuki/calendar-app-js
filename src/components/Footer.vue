9a3ab5265599d6fc4a24bc2c23fd54d791119bfc
Takaharayuuki <yuuki.barber@gmail.com>
2021-03-11 18:36:35 +0900

    フッターのtemplateの実装

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,48 @@
 <template>
-  <footer />
+  <v-footer v-if="signInUser" app class="pa-0">
+    <v-bottom-navigation grow dark>
+      <v-btn class="my-auto" height="56" @click="routerPush('/')">
+        <span>home</span>
+        <v-icon large>home</v-icon>
+      </v-btn>
+      <v-btn class="my-auto" height="56" @click="routerPush('/calendar/day')">
+        <span>daily</span>
+        <v-icon large>view_day</v-icon>
+      </v-btn>
+      <v-btn class="my-auto" height="56" @click="routerPush('/calendar/week')">
+        <span>weekly</span>
+        <v-icon large>view_week</v-icon>
+      </v-btn>
+      <v-btn class="my-auto" height="56" @click="routerPush('/calendar/month')">
+        <span>monthly</span>
+        <v-icon large>event_note</v-icon>
+      </v-btn>
+      <v-menu top open-on-hover offset-y>
+        <template v-slot:activator="{ on }">
+          <div :class="$style.profile" class="py-1 px-1 text-center" v-on="on">
+            <v-avatar :color="signInUser.themeColor">
+              <span class="white--text body-2" v-text="signInUser.nickname" />
+            </v-avatar>
+          </div>
+        </template>
+        <v-card>
+          <v-list>
+            <v-list-item v-for="(item, index) in items" :key="index" link @click="handleMethod(item.methodName)">
+              <v-list-item-icon>
+                <v-icon v-text="item.icon"></v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title v-text="item.title"></v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+          <v-card-actions>
+            <v-btn block @click="signOut">サインアウト</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-menu>
+    </v-bottom-navigation>
+  </v-footer>
 </template>
 
 <script lang="ts">
@@ -8,4 +51,9 @@ import { defineComponent } from '@vue/composition-api';
 export default defineComponent({});
 </script>
 
-<style></style>
+<style lang="scss" module>
+.profile {
+  cursor: pointer;
+  width: 168px;
+}
+</style>
